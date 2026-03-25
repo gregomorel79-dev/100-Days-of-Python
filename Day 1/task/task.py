15df97aa20bed44df6a58d522462b5293254de8a
gregomorel99-dev <gregomorel79@gmail.com>
2026-03-24 20:47:18 -0400

    first week
    My first 6 days coding

---
+++ Day 1/task/task.py
@@ -0,0 +1,6 @@
+print("will maricon")
+city = input("cual es el nombre de tu ciudad?")
+pet = input("cual es el nombre de tu mascota?")
+print("el nombre de tu banda seria: " + city + " " + pet)
+
+
