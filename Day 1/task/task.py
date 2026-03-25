2d8c9fd7cd3e86b5a26abe5e887d6a37e5900089
gregomorel99-dev <gregomorel79@gmail.com>
2026-03-24 21:10:33 -0400

    Limpieza de duplicados

--- Day 1/task/task.py
+++
@@ -1,6 +0,0 @@
-print("will maricon")
-city = input("cual es el nombre de tu ciudad?")
-pet = input("cual es el nombre de tu mascota?")
-print("el nombre de tu banda seria: " + city + " " + pet)
-
-
