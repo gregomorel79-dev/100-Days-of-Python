28b8d2cfc7590565a8ba9a64ec946ef64e39f046
gregomorel99-dev <gregomorel79@gmail.com>
2026-03-25 21:41:51 -0400

    Tip calculator project
    day3

--- 100 Days of Code - The Complete Python Pro Bootcamp/Day 2/Data Types/task.py
+++ 100 Days of Code - The Complete Python Pro Bootcamp/Day 2/Data Types/task.py
@@ -1,1 +1,8 @@
-len("Hello")
+print("hello"[-3])
+print ("hello"[-4])
+
+
+print(453_434_343)
+
+print(3.14323)
+
--- 100 Days of Code - The Complete Python Pro Bootcamp/Day 2/Type Error, Checking and Conversion/task.py
+++ 100 Days of Code - The Complete Python Pro Bootcamp/Day 2/Type Error, Checking and Conversion/task.py
@@ -1,1 +1,6 @@
-len(12345)
+name_of_the_user = input("Enter your name")
+lenght_of_name = len(name_of_the_user)
+
+
+print(type(name_of_the_user))
+print(type(lenght_of_name))
